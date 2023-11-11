f66bbe1c3ac371270dd40c894b88678b9b3afb8d
laurent <laurent.copey@gmail.com>
2023-11-11 18:48:30 +0100

    debug

--- docs/autoreload.py
+++ docs/autoreload.py
@@ -1,9 +1,10 @@
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+import os
 import time
 from datetime import datetime, timedelta
-import os
 from pathlib import Path
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 
 class SphinxHandler(FileSystemEventHandler):
@@ -26,19 +27,17 @@ class SphinxHandler(FileSystemEventHandler):
         if delta > timedelta(seconds=1) and self.in_patterns(event.src_path):
             self.last_modified = datetime.now()
             print(event)
-            os.system('make html')
+            os.system("make html")
 
 
 class Watcher:
-
     def __init__(self, root=".", filters: str | list[str] | None = None):
         self.observer = Observer()
         self.handler = SphinxHandler(root=root, patterns=filters)
         self.directory = root
 
     def run(self):
-        self.observer.schedule(
-            self.handler, self.directory, recursive=True)
+        self.observer.schedule(self.handler, self.directory, recursive=True)
         self.observer.start()
         print("\nWatcher Running in {}\n".format(self.directory))
         try:
@@ -50,7 +49,6 @@ class Watcher:
         print("\nWatcher Terminated\n")
 
 
-if __name__ == '__main__':
-    source_watcher = Watcher('../',
-                             filters=['src/**/*.py', 'docs/source/*.rst'])
+if __name__ == "__main__":
+    source_watcher = Watcher("../", filters=["src/**/*.py", "docs/source/*.rst"])
     source_watcher.run()
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,37 +11,39 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from sphinx.application import Sphinx
 
-sys.path.append('../')
+sys.path.append("../")
 
-project = 'sphinx-test'
-copyright = '2023, Laurent Copey'
-author = 'Laurent Copey'
-release = '0.1.0'
+project = "sphinx-test"
+copyright = "2023, Laurent Copey"
+author = "Laurent Copey"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ["sphinx.ext.napoleon", "myst_parser", "sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
 
 from sphinx.util.logging import getLogger
 
 
 def processor(app, what, name, obj, options, lines):
     logging = getLogger(__name__)
-    logging.info(f'Processing {what} {name}', )
-    logging.info(f'{options}')
+    logging.info(
+        f"Processing {what} {name}",
+    )
+    logging.info(f"{options}")
     return False
 
 
 def setup(app: Sphinx):
     # app.add_event('test')
-    app.connect('autodoc-process-docstring', processor)
+    app.connect("autodoc-process-docstring", processor)
